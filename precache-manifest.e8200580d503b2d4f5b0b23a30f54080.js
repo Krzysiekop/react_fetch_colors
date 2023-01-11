6101cd96528950c98cfb5159791be99de79c6c8a
Krzysiekop <krzysiek.kl.50@gmail.com>
2023-01-11 17:46:33 +0100

    Updates

---
+++ precache-manifest.e8200580d503b2d4f5b0b23a30f54080.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c608944356fd66f79bec606e92a7e868",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f4137fef169f6427b3c7",
+    "url": "./static/css/main.356a663c.chunk.css"
+  },
+  {
+    "revision": "d673c5ff98567fda4b53",
+    "url": "./static/js/2.e73cfc61.chunk.js"
+  },
+  {
+    "revision": "f4137fef169f6427b3c7",
+    "url": "./static/js/main.2cfca4fe.chunk.js"
+  },
+  {
+    "revision": "c975e425c279f8705e73",
+    "url": "./static/js/runtime~main.3f6b07f1.js"
+  }
+]);
